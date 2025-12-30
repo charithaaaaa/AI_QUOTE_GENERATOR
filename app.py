4750e24bac7cdf7b774c394dd130fde61a4c7a72
Charitha <charithakottu@gmail.com>
2025-12-30 14:47:20 +0530

    Version 2

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
 from google import genai
+from dotenv import load_dotenv
+import os
 app=Flask(__name__)
 #Add gemini key
-client = genai.Client(api_key="AIzaSyCBejYFbe6YbrFwVvckTbRn1K2HODpapyU")
+client = genai.Client(api_key=os.getenv("api"))
 @app.route("/", methods=["GET", "POST"])
 def index():
-    quote= ""
+    quotes= []
     if request.method == "POST":
         topic = request.form["topic"]
+        prompt = f"""Generate a motivational quotes about {topic}.
+        each quote should be on a new line."""
         # Generate a response using Gemini
         response = client.models.generate_content(
             model="models/gemini-flash-latest",
-            contents=f"Genertate a motivational quote about {topic}"
+            contents=prompt
         )
-        quote = response.text
-    return render_template("index.html", quote=quote)
+        quotes = response.text.strip().split("\n")
+    return render_template("index.html", quotes=quotes)
 if __name__ == "__main__":
     app.run(debug=True)
